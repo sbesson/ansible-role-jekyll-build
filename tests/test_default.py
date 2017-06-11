894bdb520db4384014de193a521de8d221c2e853
Sebastien Besson <seb.besson@gmail.com>
2017-06-11 22:15:46 +0100

    Expand test coverage

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,9 +4,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_hosts_file(File):
-    f = File('/etc/hosts')
+def test_package(host):
+    assert host.package("rubygems").is_installed
 
+
+def test_index(host):
+    f = host.file("/var/www/localhost/html/index.html")
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.is_file
