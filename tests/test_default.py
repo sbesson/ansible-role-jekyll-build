382924ef676b43f4ab500570d973b90e6ab7f7f0
Sebastien Besson <seb.besson@gmail.com>
2017-06-11 22:23:48 +0100

    Use File and Package fixtures

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,11 +4,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_package(host):
-    assert host.package("rubygems").is_installed
+def test_package(Package):
+    assert Package("rubygems").is_installed
 
 
-def test_index(host):
-    f = host.file("/var/www/localhost/html/index.html")
+def test_index(File):
+    f = File("/var/www/localhost/html/index.html")
     assert f.exists
     assert f.is_file
