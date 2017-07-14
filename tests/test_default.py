86850a7d5b18de2e4fe96b298804ff251ade481f
Sebastien Besson <seb.besson@gmail.com>
2017-07-14 10:56:20 +0100

    Revert default test

--- tests/test_default.py
+++ tests/test_default.py
@@ -2,19 +2,13 @@ import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
-FILEPATH = '/var/www/localhost/html/index.html'
-PREFIX = '\"/www.openmicroscopy.org'
 
 
 def test_package(Package):
     assert Package("rubygems").is_installed
 
 
-def test_prefix(File, TestinfraBackend):
-    host = TestinfraBackend.get_hostname()
-    f = File(FILEPATH)
+def test_index(File):
+    f = File("/var/www/localhost/html/index.html")
     assert f.exists
-    if host == 'jekyll-build-dual-config':
-        assert PREFIX not in f.content_string
-    else:
-        assert PREFIX not in f.content_string
+    assert f.is_file
